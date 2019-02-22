2bc651939eb5710c6debfe0c67936d55ed18d00c
tpedrero <tony.pedrero@etu.u-bordeaux.fr>
2019-02-22 18:14:50 +0100

    add etat

---
+++ Etat/src/etat/Etat.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etat;
+
+/**
+ *
+ * @author tpedrero
+ */
+public interface Etat {
+
+    public void afficherInfo();
+    
+    public void afficherCommande();
+    
+    public void envoyerCommande( int commande);
+    
+}
---
+++ Etat/src/etat/EtatDemarrage.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etat;
+
+/**
+ *
+ * @author tpedrero
+ */
+public class EtatDemarrage implements Etat {
+    @Override
+    public void afficherInfo(){
+        
+    }
+    
+    @Override
+    public void afficherCommande(){
+        
+    }
+    
+    @Override
+    public void envoyerCommande( int commande){
+        
+    }
+}
